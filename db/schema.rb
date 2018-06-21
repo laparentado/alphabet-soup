848ef4bfea799c7c74cd2a9a46e0a06a8a9950c1
AaronHale1989 <halea02130@gmail.com>
2018-06-21 17:39:41 -0400

    word model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_203022) do
+ActiveRecord::Schema.define(version: 2018_06_21_212709) do
 
   create_table "scores", force: :cascade do |t|
     t.integer "points"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2018_06_19_203022) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
