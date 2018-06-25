72fe7edab3cb3320e8f2cf64593ebd0be2e30d2a
Lori Aparentado <laparentado@gmail.com>
2018-06-25 11:26:40 -0400

    trying to fix edit, no dice

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    get 'edit password', to: 'devise/passwords#edit'
 
     authenticated  do
       root to: 'home#landingpage'
